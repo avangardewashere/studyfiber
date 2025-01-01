abf977e6cc1cf9c083d0cb9393cf380e94a91190
avangardewashere <avelpanaligan@gmail.com>
2025-01-01 22:43:41 +0800

    add/mesh-object-camera-light

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import { Canvas } from "@react-three/fiber";
-import "./index.css"
+import "./index.css";
 function App() {
   return (
     <>
-      <span>hello</span>
-      <div id="canvas-container"  >
-        <Canvas style={{ backgroundColor: "deepskyblue" }}>
-          <span>Hello</span>
+      {/* <span>hello</span> */}
+      <div id="canvas-container">
+        <Canvas 
+      camera={{position:[2,4,4]}}
+         >
+          <mesh>
+          {/* <sphereGeometry   args={[3,10,8]}/> */}
+          <torusKnotGeometry  args={[1.7,0.3,256,256]}/>
+          {/* <boxGeometry args={[10,10 ,11]}/> */}
+          <meshToonMaterial    />
+          <directionalLight position={[2,5,3]} />
+
+        </mesh>
         </Canvas>
       </div>
     </>
