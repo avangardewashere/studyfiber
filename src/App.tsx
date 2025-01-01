94a4aee2b54a5ce4204a6f2e359a190ebf018caf
avangardewashere <avelpanaligan@gmail.com>
2025-01-01 22:29:10 +0800

    done/with-part2

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
- 
+import { Canvas } from "@react-three/fiber";
+import "./index.css"
 function App() {
- 
   return (
     <>
-      <span>Hello</span>
+      <span>hello</span>
+      <div id="canvas-container"  >
+        <Canvas style={{ backgroundColor: "deepskyblue" }}>
+          <span>Hello</span>
+        </Canvas>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
